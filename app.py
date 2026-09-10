47ef8dd1a3602b91bf0e6f648d785989a003d5b8
AneesAhmedL <adisechatbot@gmail.com>
2026-09-10 22:28:09 +0530

    Update SMTP config and error logging

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ app.secret_key = os.getenv("SECRET_KEY", "adise_production_secure_secret_key_202
 # --- Flask-Mail Configuration ---
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
