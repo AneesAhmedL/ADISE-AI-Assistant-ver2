b6a6da3b4b3d8cabe5e424cbf25bcc0a7011a0fb
AneesAhmedL <adisechatbot@gmail.com>
2026-09-10 22:10:45 +0530

    Fix mail configuration with async thread and port 587

--- app.py
+++ app.py
@@ -21,9 +21,10 @@ app.secret_key = os.getenv("SECRET_KEY", "adise_production_secure_secret_key_202
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME", "adisechatbot@gmail.com")
-app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD", "your_app_password")
-
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
 mail = Mail(app)
 client = genai.Client()
 
